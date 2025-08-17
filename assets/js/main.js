0cac5a6724fd82080083c07ea3ba05859bf23742
MontagemdeMoveisReal <montagemdemoveis.real@gmail.com>
2025-08-17 14:22:10 -0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Script simples para destacar botão do WhatsApp
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".btn-whatsapp");
+  btn.addEventListener("click", () => {
+    console.log("Usuário clicou no botão de WhatsApp");
+  });
+});
